e3ede9ce257ac1e2c7e6e09021adc24fcc9d2998
Rodrigo Lima <rodrigofflima@hotmail.com>
2016-11-27 13:59:13 -0200

    capitulo04-aula02 [v2]

--- app/pages/generated-test/generated-test.ts
+++ app/pages/generated-test/generated-test.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ConnectionService} from './../../providers/connection-service/connection-service';
 
 @Component({
   templateUrl: 'build/pages/generated-test/generated-test.html',
 })
 export class GeneratedTestPage {
-  constructor(private nav: NavController) {}
+  constructor(private nav: NavController, private connectionService : ConnectionService) {}
+
+  buscarCep() : void{
+    this.connectionService.getCEP('19035275')
+      .then((res) =>{
+        let json = res.json();
+        console.log(json);
+      }).catch((err)=>{
+        console.log(err);
+      });
+  }
+
 }
